1a7d4a41baad56e91a054ba27739f43379600224
Thường Tôn <thuonglovevietnam@gmail.com>
2020-02-15 11:26:06 +0700

    some message

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://thuongzoo123:<password>@cluster0-8eofq.azure.mongodb.net/my-cv?retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://thuongzoo123:thuong123@cluster0-8eofq.azure.mongodb.net/my-cv?retryWrites=true&w=majority';
 module.exports = {
     mongoURI: dbPassword
 };
