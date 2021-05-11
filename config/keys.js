698d80451da9cf64c7ce0a88440436868e0d466b
thuongton98 <thuonglovevietnam@gmail.com>
2021-05-11 20:52:25 +0700

    t

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://thuongzoo123:thuong123@cluster0-8eofq.azure.mongodb.net/my-cv?retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://thuongadmin:thuong123@cluster0.gt3dp.mongodb.net/my-cv?retryWrites=true&w=majority';
 module.exports = {
     mongoURI: dbPassword
 };
